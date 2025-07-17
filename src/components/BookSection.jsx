f05457e9ee181bda0ef7701ac7d906011fbbe720
fabio-lasalvia <99scorpione@gmail.com>
2025-07-18 00:56:17 +0200

    Avviamento chiamate fetch per commenti

--- src/components/BookSection.jsx
+++ src/components/BookSection.jsx
@@ -1,32 +1,111 @@
-import React from "react";
-import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Container, Row, Col, Spinner } from "react-bootstrap";
 
 function BookSection({ books, category }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  const [selected, setSelected] = useState("");
+
+  const [comments, setComments] = useState([]);
+
+  const API_URL = "https://striveschool-api.herokuapp.com/api/comments/";
+
+  useEffect(() => {
+    if (!selected) return;
+
+    setIsLoading(true);
+    setError(false);
+
+    fetch(API_URL + selected, {
+      headers: {
+        Authorization: "Bearer ",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Errore con la richiesta");
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setComments(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      })
+      .finally(() => setIsLoading(false));
+  }, [selected]);
+
+  if (isLoading)
+    return <Spinner animation="border" className="m-5 d-block mx-auto" />;
+
+  if (error)
     return (
-        <>
-            <h1 className="m-4 text-center">{category}</h1>
+      <div className="text-center text-danger fw-bold m-5">
+        Errore nel caricamento commenti
+      </div>
+    );
 
-            <Container>
-                <Row className="g-3">
-                    {books.map((book) => (
-                        <Col key={book.asin} xs={12} sm={6} md={4} lg={3} >
-                            <Card className="mb-3 h-100">
-                                <Card.Img variant="top" src={book.img} alt={book.title} style={{ height: '400px', width: '100%', objectFit: 'cover' }} className="img-fluid" />
-                                <Card.Body className="d-flex flex-column">
-                                    <Card.Title title={book.title} className="text-truncate">{book.title}</Card.Title>
-                                    <Card.Text className="text-center fst-italic fw-semibold text-primary">Prezzo: € {book.price}</Card.Text>
+  return (
+    <>
+      <h1 className="m-4 text-center">{category}</h1>
+      <Container>
+        <Row className="g-3">
+          {books.map((book) => (
+            <Col key={book.asin} xs={12} sm={6} md={4} lg={3}>
+              <Card className="mb-3 h-100">
+                <Card.Img
+                  variant="top"
+                  src={book.img}
+                  alt={book.title}
+                  style={{ height: "400px", width: "100%", objectFit: "cover" }}
+                  className={`img-fluid ${
+                    selected === book.asin
+                      ? "border border-3 border-danger"
+                      : ""
+                  } `}
+                  onClick={() => setSelected(book.asin)}
+                />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title title={book.title} className="text-truncate">
+                    {book.title}
+                  </Card.Title>
+                  <Card.Text className="text-center fst-italic fw-semibold text-primary">
+                    Prezzo: € {book.price}
+                  </Card.Text>
 
-                                    <Button variant="primary" className="mt-auto">Aggiungi</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-
-                </Row>
-            </Container>
-        </>
-    )
+                  <Button variant="primary" className="mt-auto">
+                    Aggiungi
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      {selected && (
+        <Container className="mt-4">
+          <h3 className="text-center">Commenti</h3>
+          {isLoading ? (
+            <Spinner animation="border" className="d-block mx-auto my-4" />
+          ) : error ? (
+            <div className="text-danger text-center fw-bold">
+              Errore nel caricamento commenti
+            </div>
+          ) : comments.length > 0 ? (
+            comments.map((comment) => (
+              <p key={comment._id} className="text-center">
+                {comment.comment}
+              </p>
+            ))
+          ) : (
+            <p className="text-center">Nessun commento</p>
+          )}
+        </Container>
+      )}
+    </>
+  );
 }
 
-export default BookSection
+export default BookSection;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,45 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom'; // Outlet è il segnaposto dove verranno renderizzate le route figlie (routes annidate o nested routes)
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+import { Outlet } from "react-router-dom"; // Outlet è il segnaposto dove verranno renderizzate le route figlie (routes annidate o nested routes)
 
 import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 
 function MainLayout() {
 
-    // Tema
-    const [temaChiaro, setTemaChiaro] = useState(() => {
-        const salvato = localStorage.getItem("temaChiaro");
-        return salvato === null ? true : salvato === "true";
-    });
+  // Tema
+  const [temaChiaro, setTemaChiaro] = useState(() => {
+    const salvato = localStorage.getItem("temaChiaro");
+    return salvato === null ? true : salvato === "true";
+  });
 
-    useEffect(() => {
-        localStorage.setItem("temaChiaro", temaChiaro);
-        document.body.classList.remove("temaScuro", "temaChiaro");
-        document.body.classList.add(temaChiaro ? "temaChiaro" : "temaScuro");
-    }, [temaChiaro]);
+  useEffect(() => {
+    localStorage.setItem("temaChiaro", temaChiaro);
+    document.body.classList.remove("temaScuro", "temaChiaro");
+    document.body.classList.add(temaChiaro ? "temaChiaro" : "temaScuro");
+  }, [temaChiaro]);
 
-    // Filtro di ricerca
-    const [filtroRicerca, setFiltroRicerca] = useState("")
+  // Filtro di ricerca
+  const [filtroRicerca, setFiltroRicerca] = useState("");
 
-    return (
-        <div className="d-flex flex-column min-vh-100">
-            <Topbar
-                toggleTema={() => setTemaChiaro(!temaChiaro)}
-                onSearchChange={setFiltroRicerca}
-            />
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Topbar
+        toggleTema={() => setTemaChiaro(!temaChiaro)}
+        onSearchChange={setFiltroRicerca}
+      />
 
-            <Container fluid className="flex-grow-1">
-                <Row className="h-100">
-                    <Col>
-                        <Outlet context={{filtroRicerca, temaChiaro}} />
-                    </Col>
-                </Row>
-            </Container>
+      <Container fluid className="flex-grow-1">
+        <Row className="h-100">
+          <Col>
+            <Outlet context={{ filtroRicerca, temaChiaro }} />
+          </Col>
+        </Row>
+      </Container>
 
-            <Footer />
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
 export default MainLayout;
